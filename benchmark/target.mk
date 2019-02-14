506f08133027c7ccd6db3d4f165ff5df016e7231
Kihong Heo <kheo@cis.upenn.edu>
2019-02-14 14:55:35 -0500

    [Initialize] add benchmarks

---
+++ benchmark/target.mk
@@ -0,0 +1,16 @@
+all: lib
+
+lib:
+	make -C $(CHISEL_BENCHMARK_HOME)/benchmark/lib
+
+reduce:
+	cp $(SRC).origin.c $(SRC)
+	$(CC) -w $(SRC) -o $(ORIGIN_BIN) $(CFLAGS) $(LFLAGS)
+	chisel ./$(ORACLE) $(SRC)
+	cp $(SRC).chisel.c $(SRC).reduced.c
+
+clean:
+	rm -f $(NAME).origin $(NAME).reduced $(NAME).c
+
+distclean: clean
+	rm -rf chisel-out
